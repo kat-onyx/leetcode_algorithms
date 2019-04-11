22fa47253c41434e4936998839828476d3afffa4
kat <sarah.kathryn.peters@gmail.com>
2019-04-11 14:06:43 -0700

    Fixing preorder traversal solution

--- medium_problems/preorderTraversal.js
+++ medium_problems/preorderTraversal.js
@@ -30,3 +30,29 @@ var preorderTraversal = function(root, arr = []) {
 };
 
 //Runtime: 72 ms, faster than 27.01% of JavaScript online submissions for Binary Tree Preorder Traversal.
+
+//iterative solution
+var preorderTraversal = function (root) {
+  let result = [];
+
+  if (root === null) {
+    return result;
+  }
+
+  let stack = [root];
+
+  while (stack.length > 0) {
+    root = stack.shift();
+    result.push(root.val);
+    if (root.right != null) {
+      stack.unshift(root.right);
+    }
+    if (root.left != null) {
+      stack.unshift(root.left);
+    }
+  }
+
+  return result;
+};
+
+//Runtime: 72 ms, faster than 27.01% of JavaScript online submissions for Binary Tree Preorder Traversal.
